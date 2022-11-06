7392750defb314a14c40fae0ee253bae2559f6ee
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2022-11-06 21:57:05 +0900

    Create the first NodeJS server.
    - Wetube
    - created the server and first index.

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    next();
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handelPretected = function (req, res) {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handelPretected);
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
