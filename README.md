ca6f96e54995dbc9e3edbd2ca12578a52ad16140
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2022-11-07 23:38:00 +0900

    Making the Server.
    - using Router();

---
+++ README.md
@@ -0,0 +1,18 @@
+#Wetube project.
+
+<글로벌 라우터>
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+<유저 라우터>
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+<비디오 라우>
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete A Comment of d Video
--- src/server.js
+++ src/server.js
@@ -1,34 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    next();
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-const handelPretected = function (req, res) {
-  return res.send("Welcome to the private lounge");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handelPretected);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT}`);
